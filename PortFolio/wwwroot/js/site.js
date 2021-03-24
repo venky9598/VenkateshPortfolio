51c8990158944c3a4dc55ec87d7fa4734c9a85b1
Devanshu Agarwal <rr518074@prscoad.com>
2021-03-24 20:04:03 +0530

    First Commit

---
+++ PortFolio/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(document).ready(function () {
+    $('.mode').on('click', function () {
+        if (!$('.my-nav').hasClass('app')) {
+            $('.my-nav').addClass("app");
+            $('.my-nav').addClass("dark-mode");
+            document.documentElement.classList.add('dark')
+        }
+        else {
+            $('.my-nav').removeClass("app");
+            $('.my-nav').removeClass("dark-mode");
+            document.documentElement.classList.remove('dark')
+        }
+    });
+});
+
+
