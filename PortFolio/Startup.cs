b77494a2dbf16a829ad06041f105c26d51c67847
Devanshu Agarwal <rr518074@prscoad.com>
2021-03-27 17:29:07 +0530

    Moved UseResponseCompression

--- PortFolio/Startup.cs
+++ PortFolio/Startup.cs
@@ -55,16 +55,16 @@ namespace PortFolio
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseResponseCompression();// (Configure Method)
+
             app.UseHttpsRedirection();
             app.UseWebOptimizer();
             app.UseStaticFiles();
-
-            
             app.UseRouting();
-
             app.UseAuthorization();
             app.UseMvc();
-            app.UseResponseCompression();// (Configure Method)
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
